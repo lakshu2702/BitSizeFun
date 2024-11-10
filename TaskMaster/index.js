8b103f87da28641ad797723844e1c9f7594bd90a
Lakshmi N <161922984+lakshu2702@users.noreply.github.com>
2024-11-10 11:55:23 +0530

    Taskmaster Addition

---
+++ TaskMaster/index.js
@@ -0,0 +1,55 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import {
+    getDatabase,
+    ref,
+    push,
+    onValue,
+    remove
+} from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
+
+const firebaseConfig = {
+    databaseURL: "https://taskmaster-86153-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const referenceInDB = ref(database, "chores");
+
+const choreInputEl = document.getElementById("chore-input");
+const addButtonEl = document.getElementById("add-button");
+const deleteAllButtonEl = document.getElementById("delete-all-button");
+const ulEl = document.getElementById("chores-list");
+
+function render(chores) {
+    let listItems = "";
+    for (let i = 0; i < chores.length; i++) {
+        listItems += `
+            <li>${chores[i]}</li>
+        `;
+    }
+    ulEl.innerHTML = listItems;
+}
+
+onValue(referenceInDB, function (snapshot) {
+    const snapshotDoesExist = snapshot.exists();
+    if (snapshotDoesExist) {
+        const snapshotValues = snapshot.val();
+        const chores = Object.values(snapshotValues);
+        render(chores);
+    }
+});
+
+deleteAllButtonEl.addEventListener("dblclick", function () {
+    remove(referenceInDB)
+    ulEl.innerHTML = "";
+});
+
+addButtonEl.addEventListener("click", function () {
+    const choreText = choreInputEl.value.trim();
+    if (choreText !== "") {
+        push(referenceInDB, choreText);
+        choreInputEl.value = "";
+    } else {
+        alert("Please enter a valid chore!");
+    }
+});
